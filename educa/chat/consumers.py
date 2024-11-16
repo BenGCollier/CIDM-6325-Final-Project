dd6a49ea1a2c61c076d877743fe0be43bc55e081
Ben Collier <benjamincollier18@icloud.com>
2024-11-16 06:58:29 -0600

    Chapter 16

--- educa/chat/consumers.py
+++ educa/chat/consumers.py
@@ -1,42 +1,18 @@
 import json
 
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
+class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
 
-    async def disconnect(self, close_code):
-        # leave room group
-        await self.channel_layer.group_discard(
-            self.room_group_name, self.channel_name
-        )
-
-    # persist message to database
-    async def persist_message(self, message):
-        await Message.objects.acreate(
-            user=self.user, course_id=self.id, content=message
-        )
+    def disconnect(self, close_code):
+        pass
 
     # receive message from WebSocket
-    async def receive(self, text_data):
+    def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        now = timezone.now()
         # send message to room group
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message,
-                'user': self.user.username,
-                'datetime': now.isoformat(),
-            },
-        )
-        # persist message
-        await self.persist_message(message)
-
-    # receive message from room group
-    async def chat_message(self, event):
-        # send message to WebSocket
+        self.send(text_data=json.dumps({'message': message}))
